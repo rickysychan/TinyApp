8f41011820dc2c8cc80820937fafa6ea2c234346
Ricky Chan <rickysychan@gmail.com>
2017-09-13 13:12:01 +0000

    all activities for day 2 complete

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs")
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +24,40 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  let result = generateRandomString(6, possibleValues)
+  urlDatabase[result] = req.body.longURL
+  res.redirect("http://localhost:8080/urls/"+ result);
+});
+
+app.get("/urls/:shortUrl", (req, res) => {
+  let longURL = urlDatabase[req.params.shorUrl]
+  res.redirect(longURL);
+  console.log(url)
+});
+
+const possibleValues = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+function generateRandomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i){
+      result += chars[Math.floor(Math.random() * chars.length)];}
+    return result;
+}
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
+});
+
---
+++ test.js
@@ -0,0 +1,6 @@
+object = { "key1": "moo",
+           "key2:": "poo"}
+
+object["key3"] = "noo"
+
+console.log(object)
